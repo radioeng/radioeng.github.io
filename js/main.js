38f5ba142252e6c0cdfaf8d596d81bbec52e886c
radioeng <radio.engeneer@gmail.com>
2019-03-05 19:22:36 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,17 +36,17 @@ const terminal = new BluetoothTerminal();
 
 // Override `receive` method to log incoming data to the terminal.
 terminal.receive = function(data) {
-//	let str = data;
-//	var regex = /PING=(\d{1,10})/;
-//	var result = str.match(regex);
+	let str = data;
+	var regex = /PING=(\d{1,10})/;
+	var result = str.match(regex);
 	
-//	if(result[1] != '')
-//	{
-//		pingField.value = result[1] + ' ms';
-//	}
-
-	logToTerminal(data, 'in');
- 
+	if(result[1] != '')
+	{
+		pingField.value = result[1] + ' ms';
+	}
+	else
+	{	logToTerminal(data, 'in');
+	}
 };
 
 // Override default log method to output messages to the terminal and console.
