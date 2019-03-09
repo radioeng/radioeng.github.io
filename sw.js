4aea5b6b48cb7f58e62276f9b1acc00588350ca8
radioeng <radio.engeneer@gmail.com>
2019-03-09 18:23:29 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,3 @@
-importScripts('js/sw-toolbox.js');
-
-toolbox.precache([
-  'css/normalize.css',
-  'css/styles.css',
-  'js/BluetoothConnection.js',
-  'js/companion.js',
-  'js/main.js',
-  'index.html',
-]);
-
-toolbox.router.default = toolbox.networkFirst;
-toolbox.options.networkTimeoutSeconds = 5;
-
-toolbox.router.get('icons/*', toolbox.fastest);
-
-
 'use strict';
 
 console.log('Service worker startup');
