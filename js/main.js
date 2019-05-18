52b3bb73e19ee798a46977359b25981ed5b7e350
radioeng <radio.engeneer@gmail.com>
2019-05-18 10:33:40 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -142,7 +142,7 @@ settingButton.addEventListener('click', () => {
 });
 
 pingButton.addEventListener('click', function () {
-	send('ping');
+	send('send -m -t "ttttt" -r');
 });
 
 saveButton.addEventListener('click', function () {
