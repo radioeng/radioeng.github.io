1d8e0af9ea6ba2b709ba0cf94135a18e95aa2d13
radioeng <radio.engeneer@gmail.com>
2019-05-24 13:40:16 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -128,7 +128,7 @@ settingButton.addEventListener('click', () => {
 		if(settingWindow.style.display == 'none' || settingWindow.style.display == '')
 		{
 			settingWindow.style.display = 'block';
-			send('\cc');
+			msg-start();
 			send('SETTING');
 		}
 		else
